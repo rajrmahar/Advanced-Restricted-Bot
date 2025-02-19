e09aa31a37388414dfda1d542d806205bacfc5fc
Rajesh R Mahar <136462044+rajrmahar@users.noreply.github.com>
2025-02-19 18:11:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,5 +26,5 @@ WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
 AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
 STRING = getenv("STRING", None)
 YT_COOKIES = getenv("YT_COOKIES", YTUB_COOKIES)
-DEFAULT_SESSION = getenv("DEFAUL_SESSION", "")  # added old method of invite link joining
+DEFAULT_SESSION = getenv("DEFAUL_SESSION", "AQFCJtoAG-BWr-q9nKSSBk1Juk7jQk_UoTj1PaRzSvIhdR4Y5MF2m8yCW5e5n7YHYNICqn5u6bT7U5czUX22vQwcleryhrmug4hO4qfS1twJVQEZXbU4bTQO8YWbuokH5qEAgs5HdWjIDkL0dlP-ElLQMpcn1Snd0HuD9D5st4S7C1rnZ7vvYyGtPi5Aj8SC-_0_VaQgR8_sX3sHyXIYmweVhUfj7IJ6V4CMxd-tpTsKjNRVEKXqX68qnVMcJjFG9IdeohqJh8kqPwShlDgDTwYpuQ7Ht-XEDpO-XTa9XqflTubETV7xEhu7vVsWVMnkLNuY2OvzT1HXG49H5dmqfkgSewYjtwAAAAHGQZT-AA")  # added old method of invite link joining
 INSTA_COOKIES = getenv("INSTA_COOKIES", INST_COOKIES)
